317f2cca2ae7bd4cbbf76fba7c560181a1d9265f
kvs0102013 <kvs0102013@gmail.com>
2025-01-10 21:00:42 +0500

    task_four

--- lesson2.py
+++ lesson2.py
@@ -11,6 +11,19 @@
 # b = 'Blue'
 
 # print(r, b, g, r+g+b, b, g+b)
-first_animal = "Заяц"
-second_animal = 'Черепаха'
-print(first_animal , 'спит' , second_animal , 'идет')
+
+# first_animal = "Заяц"
+# second_animal = 'Черепаха'
+# print(first_animal , 'спит' , second_animal , 'идет')
+
+first_name = input('Введите имя пользователя: ')
+
+greeting = 'Утро доброе'
+
+print(greeting, first_name)
+
+intro = "К сожалению, у Вас нет доступа к системе."
+
+info = "Пожалуйста, обратитесь к системному администратору."
+print(intro)
+print(info)
